490e75e3484a6bbf99e4d64c6da16b6bf35171f0
Vadim <uvolkov@374@gmail.com>
2022-08-11 21:34:26 +0300

    Add tasks DZ

---
+++ TASK64/Program.cs
@@ -0,0 +1,27 @@
+﻿int n = InputInt("Enter number N: ");
+int m = InputInt("Enter number M: ");
+Console.WriteLine("The train of numbers lower!)"); 
+Console.WriteLine(NaturalNumber(n,m));
+int NaturalNumber(int n, int m)
+
+{   if(n < m)
+{
+    int temp = n;
+    n = m;
+    m = temp;
+}
+    if (n == m)
+    {
+        return m;
+    }
+    else
+    {
+        Console.Write(NaturalNumber(n - 1, m) + " ");
+        return n;
+    }
+}
+int InputInt(string output)
+{
+    Console.Write(output);
+    return Convert.ToInt32(Console.ReadLine());
+}
---
+++ TASK66/Program.cs
@@ -0,0 +1,23 @@
+﻿int n = InputInt("Enter number N: ");
+int m = InputInt("Enter number M: ");
+Console.WriteLine($"We have SUM: {SumNumber(n, m)}");
+int SumNumber(int n, int m)
+{
+    
+    int sum = n;
+    if (n == m)
+    {
+        return sum;
+    }
+    else
+    {
+        return sum + SumNumber(n+1, m);
+    }
+
+}
+int InputInt(string output)
+{
+    Console.Write(output);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
